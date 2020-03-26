1c81fe8e43b941243812bdd632ed3204804ff0e4
Ahmed Nabil <ahmednabil950@gmail.com>
2020-03-26 23:45:33 +0200

    Re-write reader utilities class

--- dataset.py
+++ dataset.py
@@ -9,10 +9,12 @@ Created on Wed Jan  8 21:48:22 2020
 import tools as tools
 import torch
 import pandas as pd
+from network import EncoderRNN, DecoderRNN
+from torch import optim
 
 SOS = 0
 EOS = 1
-FILES = {'ar': 'UNv1.0.ar-en.ar.10k', 'en': 'UNv1.0.ar-en.en.10k'}
+FILES = {'ar': 'ara.txt', 'en': 'eng.txt'}
 
 
 class Dictionary:
@@ -39,7 +41,29 @@ class Dictionary:
 
 
 
-class SentenceReader:
+class Reader:
+    
+    def __init__(self, lang, path, max_len=10, min_len=1, max_chars=40):
+        self.lang = lang
+        self.path = path
+        self.sentences = []
+        self.max_len = max_len
+        self.min_len = min_len
+        self.max_chars = max_chars
+
+    def read(self):
+        print('Reading Sentences for language %s ...' %(self.lang))
+        with open(self.path, 'r') as reader:
+            self.sentences += list(map(tools.cleaner_job, reader.readlines()))
+        return self.sentences
+
+    def get_tokenized(self):
+        print('Tokenizing %s ...' %(self.lang))
+        return [[w for w in s.split()] for s in self.sentences]
+
+
+
+class SentenceReader(Reader):
     """
         * Use Set to iterate over instead of list according to this thread 
         * Python Garbage Collector Bug when list is going bigger
@@ -47,33 +71,37 @@ class SentenceReader:
     https://stackoverflow.com/questions/2473783/is-there-a-way-to-circumvent-python-list-append-becoming-progressively-slower
     
     """
-    to_remove = set()
-    to_have = set()
-    
-    def __init__(self, lang, path, max_len=100, min_len=40):
-        self.lang = lang
-        self.path = path
-        self.sentences = []
-        self.max_len = max_len
-        self.min_len = min_len
+    def __init__(self, input_lang_map, outpt_lang_map):
+        super(SentenceReader, self).__init__(input_lang_map, outpt_lang_map)
+        in_lang, in_path = tuple(*(input_lang_map.items()))
+        out_lang, out_path = tuple(*(outpt_lang_map.items()))
+        self.input = Reader(lang=in_lang, path=in_path)
+        self.outpt = Reader(lang=out_lang, path=out_path)
+        self.lang_map = {in_lang: self.input, out_lang: self.outpt}
+        self.input.read()
+        self.outpt.read()
+        self.to_remove = set()
+        self.to_have = set()
+        self.__filter_sentences()
         
+    def read_sentences(self, lang):
+        to_have = self.to_have
+        lang = self.lang_map.get(lang)
+        if lang:
+            return lang.sentences
+        else:
+            raise AttributeError("Invalid Language Attribute")
+        
+    def get_tokenized(self, lang):
+        print('Tokenizing %s ...' %(lang))
+        sentences = self.lang_map.get(lang).sentences
+        return [[w for w in s.split()] for s in sentences]
+    
     def __filter_sentences(self):
-        max_len, min_len = self.max_len, self.min_len
-        to_have = self.__class__.to_have
-        to_remove = self.__class__.to_remove
-
-        if not len(to_remove):
-            for i, sentence in enumerate(self.sentences):
-                if len(sentence.split()) > 25:
-                    to_remove.add(i)
-                    continue
-                if len(sentence) > max_len:
-                    to_remove.add(i)
-                elif len(sentence) < min_len:
-                    to_remove.add(i)
-                if i not in to_remove:
-                    to_have.add(i)
-
+        max_len, min_len, max_chars = self.max_len, self.min_len, self.max_chars
+        to_have = self.to_have
+        to_remove = self.to_remove
+        
         ######################################################################
         ## TODO: too slow should be refactored
         # s = len(self.sentences)
@@ -83,32 +111,27 @@ class SentenceReader:
         #     else:
         #         self.sentences[i] = tools.cleaner_job(self.sentences[i])
         ######################################################################
-
-        sent_series_obj = pd.Series(self.sentences)
-        self.sentences = sent_series_obj[to_have].tolist()
-
-    def read_sentences(self):
-        print('Reading Sentences for language %s ...' %(self.lang))
-        with open(self.path, 'r') as reader:
-            self.sentences += list(map(tools.cleaner_job, reader.readlines()))
-
-        self.__filter_sentences()
         
-        return self.sentences
+        for reader in [self.input, self.outpt]:
+            for i, sentence in enumerate(reader.sentences):
+                if len(sentence.split()) > max_len:
+                    to_remove.add(i)
+                elif len(sentence.split()) < min_len:
+                    to_remove.add(i)
+                if i not in to_remove:
+                    to_have.add(i)
+            to_have = [i for i in to_have if i not in to_remove]
+            sent_series_obj = pd.Series(reader.sentences)
+            reader.sentences = sent_series_obj[[i for i in to_have]].tolist()
+            to_have = set(to_have)
 
-    def get_tokenized(self):
-        print('Tokenizing %s ...' %(self.lang))
-        return [[w for w in s.split()] for s in self.sentences]
-
-
-def prepare_dataset(in_lang, out_lang):
+def prepare_dataset(in_lang_path, out_lang_path):
     print('Preparing Dataset')
     
-    in_sentences_obj = SentenceReader('en', in_lang)
-    out_sentences_obj = SentenceReader('ar', out_lang)
+    reader = SentenceReader({'en': in_lang_path}, {'ar': out_lang_path})
 
-    pairs = [(in_sent, out_sent) for (in_sent, out_sent) in zip(in_sentences_obj.read_sentences(), 
-                                                                out_sentences_obj.read_sentences())]
+    pairs = [(in_sent, out_sent) for (in_sent, out_sent) in zip(reader.read_sentences('en'), 
+                                                                reader.read_sentences('ar'))]
 
     print('Building Language Dictionary')
 
@@ -124,12 +147,12 @@ def prepare_dataset(in_lang, out_lang):
     pairs_encoded = [([in_word_to_idx[in_w]  for in_w in in_sent], 
                       [ou_word_to_idx[out_w] for out_w in out_sent]) 
                                              for (in_sent, out_sent) 
-                                             in zip(in_sentences_obj.get_tokenized(), 
-                                                    out_sentences_obj.get_tokenized())]
+                                             in zip(reader.get_tokenized('en'), 
+                                                    reader.get_tokenized('ar'))]
     
     return {
         'pairs': pairs, 
-        'pairs_index': pairs_encoded, 
+        'pairs_encoded': pairs_encoded, 
         'in_dictionary': in_dictionary, 
         'out_dictionary': ou_dictionary,
     }
@@ -163,6 +186,6 @@ def index_to_tensor(pairs):
     
     return tensors
 
-FILES_ABS_PATH = ('./data/'+FILES['en'], './data/'+FILES['ar'])
-dataset = prepare_dataset(*FILES_ABS_PATH)
-tensors = index_to_tensor(dataset['pairs_index'])
+# FILES_ABS_PATH = ('./data/'+FILES['en'], './data/'+FILES['ar'])
+# dataset = prepare_dataset(*FILES_ABS_PATH)
+# tensors = index_to_tensor(dataset['pairs_index'])
